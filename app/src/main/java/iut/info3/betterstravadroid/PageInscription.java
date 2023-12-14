38ef3da3b81e04db2b8fdf5db0d71fbcb124b26b
ClemFR <clementlaurie27@gmail.com>
2023-12-14 16:04:24 +0100

    Fix de la vérification de l'adresse email

--- app/src/main/java/iut/info3/betterstravadroid/PageInscription.java
+++ app/src/main/java/iut/info3/betterstravadroid/PageInscription.java
@@ -42,17 +42,32 @@ public class PageInscription extends AppCompatActivity {
         String password = this.password.getText().toString();
         String confirmPassword = this.confirmPassword.getText().toString();
 
+        boolean formulaireOk = true;
+
+        // Validation des champs du formulaire
         if (nom.isEmpty() || prenom.isEmpty() || courriel.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
             Toast.makeText(this, "Veuillez remplir tous les champs", Toast.LENGTH_SHORT).show();
-        } else if (!password.equals(confirmPassword)) {
-            Toast.makeText(this, "Les mots de passe ne correspondent pas", Toast.LENGTH_SHORT).show();
-        } else if (password.length() < 8) {
-            Toast.makeText(this, "Le mot de passe doit contenir au moins 8 caractères", Toast.LENGTH_SHORT).show();
-        } else if (Patterns.EMAIL_ADDRESS.matcher(courriel).matches()) {
-            Toast.makeText(this, "Le courriel n'est pas valide", Toast.LENGTH_SHORT).show();
-        } else {
+            formulaireOk = false;
+        }
 
-            // On crée le json body
+        if (formulaireOk && !password.equals(confirmPassword)) {
+            Toast.makeText(this, "Les mots de passe ne correspondent pas", Toast.LENGTH_SHORT).show();
+            formulaireOk = false;
+        }
+
+        if (formulaireOk && !Patterns.EMAIL_ADDRESS.matcher(courriel).matches()) {
+            Toast.makeText(this, "Le courriel n'est pas valide", Toast.LENGTH_SHORT).show();
+            formulaireOk = false;
+        }
+
+        if (formulaireOk && password.length() < 8) {
+            Toast.makeText(this, "Le mot de passe doit contenir au moins 8 caractères", Toast.LENGTH_SHORT).show();
+            formulaireOk = false;
+        }
+
+        if (formulaireOk) {
+
+            // les champs du formulaires sont corrects, on crée le body de la requête POST
             JSONObject body = new JSONObject();
             try {
                 body.put("nom", nom);
@@ -60,14 +75,14 @@ public class PageInscription extends AppCompatActivity {
                 body.put("email", courriel);
                 body.put("motDePasse", password);
 
-                // On fait la requête
+                // On envoie la requête
                 RequestHelper.simpleJSONObjectRequest(
                         UserApi.USER_API_BASE_URL,
                         null,
                         body,
                         Request.Method.POST,
                         response -> {
-                            Toast.makeText(this, "Inscription réussie", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(this, "Inscription réussie", Toast.LENGTH_LONG).show();
                             this.finish();
                         },
                         error -> {
